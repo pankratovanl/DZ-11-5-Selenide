03ad6ac1c10ba08e8b42dc9567226e73d070312a
Pankratova Nadezhda <pankratovanl@gmail.com>
2022-03-20 21:09:29 +0500

    added DZ Selenide#1

--- src/test/java/guru/qa/SelenideTests/SelenideTests.java
+++ src/test/java/guru/qa/SelenideTests/SelenideTests.java
@@ -4,6 +4,9 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.files.DownloadActions.click;
 
@@ -13,13 +16,27 @@ public class SelenideTests {
     void shouldFindSelenideInGithub() {
         // открыть страницу github.com
         open("http://github.com/");
+
         // ввести в поле поиска selenide и нажать Enter
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+
         //нажать на линк от первого результата поиска
         $$("ul.repo-list li").first().$("a").click();
+
         //check: в заголовке встречается selenide/selenide, кликнуть
-        $("h1").shouldHave(Condition.text("selenide / selenide")).click();
-        //
+        $("h1").shouldHave(text("selenide / selenide")).click();
+
+        //Перейти в раздел Wiki проекта
+        $("#wiki-tab.UnderlineNav-item").click();
+
+        //check: в списке странц Pages есть страница SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("[data-filterable-for=\"wiki-pages-filter\"]").$(byText("SoftAssertions")).click();
+
+        //check: внутри есть пример кода для JUnit5
+        $$("h4").findBy(text("Using JUnit5 extend test class:")).sibling(0).shouldHave(text("SoftAssertsExtension.class"));
+
+
     }
 }
 
